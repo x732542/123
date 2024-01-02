8859eb9595e4cf632262bf2c5be5585714afdae6
x732542 <154980490+x732542@users.noreply.github.com>
2024-01-02 21:26:23 +0800

    Update tokyohot.js

--- tokyohot.js
+++ tokyohot.js
@@ -23,27 +23,3 @@ btn.addEventListener("mouseout", function(){
     this.innerText = "yoooo";
     this.style.color ="blue";
 })
-
-
-    
-    
-
-// sounds.forEach((sound) => {
-//     let btn = document.createElement("button");
-//     btn.classList.add("btn");
-//     btn.innerText = sound;
-//     btn.addEventListener("click", () => {
-//       stopSounds();
-//       document.getElementById(sound).play();
-//       //上面的sound不需用字串符號包起來，因為它本身是一個變數，代表audio標籤內的id
-//     });
-//     document.getElementById("buttons").appendChild(btn);
-//   });
-      
-//   function stopSounds() {
-//     sounds.forEach((sound) => {
-//       let song = document.getElementById(sound);
-//       song.pause();
-//       song.currentTime = 0;
-//     });
-//   }
