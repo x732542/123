4f7b95719bca0490ce3082ac04ceeddcddb9957e
x732542 <154980490+x732542@users.noreply.github.com>
2024-01-16 09:38:23 +0800

    Update tokyohot.js

--- tokyohot.js
+++ tokyohot.js
@@ -1,83 +1,85 @@
-var btn=document.getElementById("tokyo");
-var audio=document.getElementById("tokyohot");
 var tag = true;
-btn.onclick = function(){
-    if(tag){
-                
-        audio.pause();
-        tag = false;
-    }else{
-        audio.play();
-        tag = true;
-            }
-     }    
-btn.addEventListener("mouseover", function(){
-    this.innerText = "yoooooo";
-    this.style.color ="red";
-})
-btn.addEventListener("mouseout", function(){
-    this.innerText = "hot";
-    this.style.color ="blue";
-})
 
-var btn=document.getElementById("rick");
-var audioo=document.getElementById("rickroll");
-btn.onclick = function(){
-    if(tag){
-                
-        audioo.pause();
+var btn1 = document.getElementById("tokyo");
+var audio1 = document.getElementById("tokyohot");
+btn1.onclick = function () {
+    if (tag) {
+        audio1.pause();
         tag = false;
-    }else{
-        audioo.play();
+    } else {
+        audio1.play();
         tag = true;
-            }
-     }    
-btn.addEventListener("mouseover", function(){
+    }
+};
+btn1.addEventListener("mouseover", function () {
+    this.innerText = "yoooooo";
+    this.style.color = "red";
+});
+btn1.addEventListener("mouseout", function () {
+    this.innerText = "hot";
+    this.style.color = "blue";
+});
+
+// 第二個按鈕
+var btn2 = document.getElementById("rick");
+var audio2 = document.getElementById("rickroll");
+btn2.onclick = function () {
+    if (tag) {
+        audio2.pause();
+        tag = false;
+    } else {
+        audio2.play();
+        tag = true;
+    }
+};
+btn2.addEventListener("mouseover", function () {
     this.innerText = "roool";
-    this.style.color ="green";
-})
-btn.addEventListener("mouseout", function(){
+    this.style.color = "green";
+});
+btn2.addEventListener("mouseout", function () {
     this.innerText = "rick roll";
-    this.style.color ="darkmagenta";
-})
-var btn=document.getElementById("no");
-var audiooo=document.getElementById("nono");
-btn.onclick = function(){
-    if(tag){
-                
-        audiooo.pause();
+    this.style.color = "darkmagenta";
+});
+
+// 第三個按鈕
+var btn3 = document.getElementById("no");
+var audio3 = document.getElementById("nono");
+btn3.onclick = function () {
+    if (tag) {
+        audio3.pause();
         tag = false;
-    }else{
-        audiooo.play();
+    } else {
+        audio3.play();
         tag = true;
-            }
-     }    
-btn.addEventListener("mouseover", function(){
+    }
+};
+btn3.addEventListener("mouseover", function () {
     this.innerText = "no";
-    this.style.color ="pink";
-})
-btn.addEventListener("mouseout", function(){
+    this.style.color = "pink";
+});
+btn3.addEventListener("mouseout", function () {
     this.innerText = "八尬nono";
-    this.style.color ="silver";
-})
-var btn=document.getElementById("bling");
-var audioooo=document.getElementById("blingbang");
-btn.onclick = function(){
-    if(tag){
-                
-        audioooo.pause();
+    this.style.color = "silver";
+});
+
+// 第四個按鈕
+var btn4 = document.getElementById("bling");
+var audio4 = document.getElementById("blingbang");
+btn4.onclick = function () {
+    if (tag) {
+        audio4.pause();
         tag = false;
-    }else{
-        audioooo.play();
+    } else {
+        audio4.play();
         tag = true;
-            }
-     }    
-btn.addEventListener("mouseover", function(){
+    }
+};
+btn4.addEventListener("mouseover", function () {
     this.innerText = "bling";
-    this.style.color ="yellow";
-})
-btn.addEventListener("mouseout", function(){
+    this.style.color = "yellow";
+});
+btn4.addEventListener("mouseout", function () {
     this.innerText = "bang-bang";
-    this.style.color ="darkblue";
-})
+    this.style.color = "darkblue";
+});
 
